2e09832ba56185395f2df5e2706194a877272428
Piotr Bogdański <pbogdansk@student.agh.edu.pl>
2023-01-17 23:56:11 +0100

    Path/endpoint parameters test

--- main.py
+++ main.py
@@ -3,13 +3,13 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Path
+from typing import Optional
 app = FastAPI()
 
 toDoList = {
     1: {
-        "Excersize name": "Bent over row",
+        "Excersize name": "test",
         "Number of sets": 5,
         "number of reps": 5
     }
@@ -18,9 +18,16 @@ toDoList = {
 
 # http://127.0.0 .1:8000/docs dokumentacja
 @app.get("/home")  # jsonowanie sobie automatycznie robi FastApi
-def trojkacik():
+def test():
     return {"test"}
 
 @app.get("/get-excersize/{excercise_id}")
-def get_excersize(excercise_id: int):
+def get_excercize(excercise_id: int = Path(None, description="The id of the excersize:")):
     return toDoList[excercise_id]
+
+@app.get("/get-by-name")
+def get_excercize(name: Optional[str] = None):
+    for excercize_id in toDoList:
+        if toDoList[excercize_id]["name"] == name:
+            return toDoList[excercize_id]
+    return {"Data": "Not found"}
