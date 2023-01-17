53a208f40373954a8d9dd3ded6df74dab23bf0c9
Piotr Bogdański <pbogdansk@student.agh.edu.pl>
2023-01-17 22:20:06 +0100

    Path/endpoint parameters test

--- main.py
+++ main.py
@@ -6,7 +6,21 @@
 from fastapi import FastAPI
 
 app = FastAPI()
-#http://127.0.0.1:8000/docs dokumentacja
-@app.get("/home")
+
+toDoList = {
+    1: {
+        "Excersize name": "Bent over row",
+        "Number of sets": 5,
+        "number of reps": 5
+    }
+
+}
+
+# http://127.0.0 .1:8000/docs dokumentacja
+@app.get("/home")  # jsonowanie sobie automatycznie robi FastApi
 def trojkacik():
-    return {"dupa"}
+    return {"test"}
+
+@app.get("/get-excersize/{excercise_id}")
+def get_excersize(excercise_id: int):
+    return toDoList[excercise_id]
