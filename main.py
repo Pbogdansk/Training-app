24d8d8f248373674ca709817f6a26a62dc8a296c
Piotr Bogdański <pbogdansk@student.agh.edu.pl>
2023-01-18 11:21:06 +0100

    added delete functionality

--- main.py
+++ main.py
@@ -6,18 +6,23 @@
 from fastapi import FastAPI, Path
 from typing import Optional
 from pydantic import BaseModel
+
 app = FastAPI()
+
 
 class Item(BaseModel):
     name: str
     reps: int
     sets: int
     load: int
+
+
 class UpdateItem(BaseModel):
     name: Optional[str] = None
     reps: Optional[int] = None
     sets: Optional[int] = None
     load: Optional[int] = None
+
 
 toDoList = {
     1: {
@@ -28,20 +33,23 @@ toDoList = {
 
 }
 
+
 # http://127.0.0 .1:8000/docs dokumentacja
 @app.get("/home")  # jsonowanie sobie automatycznie robi FastApi
 def test():
     return {"test"}
 
+
 @app.get("/get-excersize/{excercise_id}")
 def get_excercize(excercise_id: int = Path(None, description="The id of the excersize:")):
     return toDoList[excercise_id]
 
-#@app.get("/search/{item_id}")
-#def get_excercize(name: Optional[str] = None):
+
+# @app.get("/search/{item_id}")
+# def get_excercize(name: Optional[str] = None):
 #    for item_id in toDoList:
- #       if toDoList[item_id].name == name:
- #           return toDoList[item_id]
+#       if toDoList[item_id].name == name:
+#           return toDoList[item_id]
 #    return {"Data": "Not found"}
 
 @app.post("/create-item/{item_id}")
@@ -49,19 +57,26 @@ def create_item(item_id: int, item: Item):
     if item_id in toDoList:
         return {"already dodaned"}
 
-    toDoList[item_id] = {"name": item.name, "reps": item.reps, "sets": item.sets, "load": item.load}
+    toDoList[item_id] = {"name": item.name, "reps": item.reps, "sets": item.sets}
     return toDoList[item_id]
 
+
 @app.put("/update-item/{item_id}")
-def update_item(item_id: int, item: Item):
+def update_item(item_id: int, item: UpdateItem):
     if item_id not in toDoList:
         return {"Error": "Item does not exist"}
     if item.name != None:
-        toDoList[item_id].name = item.name
+        toDoList[item_id]["Excercize name"] = item.name
+
     if item.reps != None:
-        toDoList[item_id].reps = item.reps
+        toDoList[item_id]["Number of sets"] = item.reps
+
     if item.sets != None:
-        toDoList[item_id].sets = item.sets
-    if item.load != None:
-        toDoList[item_id].load = item.load
+        toDoList[item_id]["number of reps"] = item.sets
+
     return toDoList[item_id]
+@app.delete("/delete")
+def delete_item(item_id: int, item: Item):
+    if item_id not in toDoList:
+        return {"There is no such item"}
+    del toDoList[item_id]
